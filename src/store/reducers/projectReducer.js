772423dc33f38a1a63d2a256bfecceb9cb338ffb
madjack <madjack@nm.ru>
2018-11-07 17:00:40 +0400

    add connect to Dashbboar, pass the props down to ProjectList and Summary

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,21 @@
 const initState = {
-
+    projects: [
+        {
+            "id": "1",
+            "title": "help me find peach",
+            "content": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum"
+          },
+          {
+            "id": "2",
+            "title": "collect all the stars",
+            "content": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor"
+          },
+          {
+            "id": "3",
+            "title": "egg hunt with yoshi",
+            "content": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus"
+          },
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
