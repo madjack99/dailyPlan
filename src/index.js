d7e7eeac96be5d3c30f916f4882b069ee0868a3d
madjack <madjack@nm.ru>
2018-11-08 16:00:14 +0400

    Attach the project to the db by reactReduxFirebase and reduxFirestore

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ import fbConfig from './config/fbConfig'
 const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-
+        reactReduxFirebase(fbConfig),
+        reduxFirestore(fbConfig)
     ) 
 )
 
