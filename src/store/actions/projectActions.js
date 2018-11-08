ad2165fe4188fbe827205fe82b7bcdc69b356809
madjack <madjack@nm.ru>
2018-11-08 16:21:16 +0400

    invoke firestore in CreateProject and add promise to add doc. to fireSto

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
-
-        dispatch({type: 'CREATE_PROJECT', project})
+        // async call to the database
+        const firestore = getFirestore()
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'John',
+            authorLastName: 'Doe',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project})
+        }).catch((err) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', err})
+        })
     }
 }
