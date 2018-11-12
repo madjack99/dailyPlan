00fa99bbb0ec1efe6f62a09ac0529a2c116bd7db
madjack <madjack@nm.ru>
2018-11-12 18:46:50 +0400

    add action for signOut, update switch case reducer, get it in SInLinks

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -16,6 +16,9 @@ const authReducer = (state = initState, action) => {
                 ...state,
                 authError: null
             })
+        case 'LOGOUT_SUCCESS':
+            console.log('signout success')
+            return state
         default: 
             return state
     }
