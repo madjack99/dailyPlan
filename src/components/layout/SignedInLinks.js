4f8fd755e7acb312e952421b7c317bdde5fe7d99
madjack <madjack@nm.ru>
2018-11-13 17:36:51 +0400

    add store enhancer useFSForProf to sync firebase.profile with firestore

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -9,7 +9,9 @@ const SignedInLinks = (props) => {
         <ul className="right">
             <li><NavLink to='/create'>New project</NavLink></li>
             <li><a onClick={props.signOut}>Log out</a></li>
-            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>
+                {props.profile.initials}
+            </NavLink></li>
         </ul>
     )
 }
