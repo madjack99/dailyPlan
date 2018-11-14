4ef8a5a6a094492c09fb1e4b778d34be05615980
madjack <madjack@nm.ru>
2018-11-14 16:08:20 +0400

    get FName and SName for author from getState().firebase in the actions

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,12 +1,14 @@
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // async call to the database
+        const profile = getState().firebase.profile
+        const authorId = getState().firebase.auth.uid
         const firestore = getFirestore()
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'John',
-            authorLastName: 'Doe',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date()
         }).then(() => {
             dispatch({type: 'CREATE_PROJECT', project})
